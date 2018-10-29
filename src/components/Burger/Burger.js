5e9a31b727f8620518cb3c2a71a2c7e5a8d5159c
snkoski <shawnnkoski@gmail.com>
2018-10-29 18:49:48 -0400

    transformedIngredients checks if there are ingredients in state, if not print message to add ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    console.log("FIRST MAP", igKey);
     return [...Array(props.ingredients[igKey])].map((_, i) => {
+      console.log("SECOND MAP", i);
+      console.log("SECOND MAP", igKey);
       return <BurgerIngredient key={igKey + i} type={igKey} />;
     });
-  });
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please Start Building Your Burger</p>
+  }
 
+  console.log("TRANSFORMED", transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
